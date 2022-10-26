f3bd220f222515e242459be3610e489ac01f5017
jamesdev23 <jnrara0023@gmail.com>
2022-10-26 20:47:24 +0800

    updating Activity01I.kt and Activity01J.kt

--- src/main/kotlin/activity_01_i/Activity01I.kt
+++ src/main/kotlin/activity_01_i/Activity01I.kt
@@ -1,2 +1,136 @@
 package activity_01_i
+import java.time.LocalDate
+import java.time.Period
 
+// Borrowing book and check record. User: Librarian
+
+fun main(){
+    var optionsInput:String? = null
+    var optionYesNo:String? = null
+    var bookBorrowerName:String? = null
+    var bookTitle:String? = null
+    var borrowDuration:Int = 0
+    var borrowDateEnd:String? = null
+    var borrowerInfoColumn = arrayListOf("Name","Type","Book Borrowed","Duration (in days)","Borrowed on","Return Date")
+    var borrowerInfo = arrayListOf("James","Student","The Scarlet Letter","7","2022-10-22","2022-10-29")
+    var bookBorrowDateStart = LocalDate.now()
+    var bookBorrowed = arrayListOf("The Scarlet Letter")
+    var borrowerType = arrayOf(
+        "Visitor",
+        "Student",
+        "Faculty"
+    )
+
+    var borrowerTypeBorrowDuration = arrayOf(
+        "room-use",
+        "7 days",
+        "30 days"
+    )
+    var bookList = arrayListOf<String>(
+        "Don Quixote",
+        "Alice\'s Adventures in Wonderland",
+        "The Adventures of Huckleberry Finn",
+        "The Adventures of Tom Sawyer",
+        "Treasure Island",
+        "Pride and Prejudice",
+        "Wuthering Heights",
+        "Jane Eyre",
+        "Moby Dick",
+        "The Scarlet Letter"
+    )
+
+    var bookAuthor = arrayListOf<String>(
+        "Miguel de Cervantes",
+        "Lewis Carroll",
+        "Mark Twain",
+        "Mark Twain",
+        "Robert Louis Stevenson",
+        "Jane Austen",
+        "Emily Brontë",
+        "Charlotte Brontë",
+        "Herman Melville",
+        "Nathaniel Hawthorne"
+    )
+    var canBorrow = true
+    var counter = 0
+    var column = 0
+
+    println("===============")
+    println("Librarian Menu: ")
+    println("===============")
+    println("- Borrow a Book")
+    println("- Borrowed Book Record")
+    println("Proceed to borrowing book...")
+
+    while(canBorrow == true) {
+        println("Enter Borrower's Name: ")
+        bookBorrowerName = readLine().toString()
+        borrowerInfo.add(bookBorrowerName)
+        println("Enter Borrower Type (visitor,student,faculty)")
+        var tempBorrowerType = readLine().toString()
+        borrowerInfo.add(tempBorrowerType)
+
+        if (tempBorrowerType == "visitor"){
+            println("Note: Any books borrowed by visitor is for room-use only, within 1 day.")
+            borrowDuration = 1
+        } else if (tempBorrowerType == "student") {
+            println("Note: Students can borrow a book up to 7 days")
+            borrowDuration = 7
+        } else if (tempBorrowerType == "faculty") {
+            println("Note: Faculty and other school staff can borrow a book up to 30 days")
+            borrowDuration = 30
+        } else {
+            println("Error: Not a valid option")
+        }
+        println("Book Title: ")
+        bookTitle = readLine().toString()
+
+        // simple check if it can be borrowed
+        if (bookBorrowed.contains(bookTitle)) {
+            println("This book can not be borrowed.")
+        } else {
+            println("This book can be borrowed.")
+            borrowerInfo.add(bookTitle)
+            var durationStr = borrowDuration.toString()
+            borrowerInfo.add(durationStr)
+            borrowerInfo.add(bookBorrowDateStart.toString())
+            var endDate = bookBorrowDateStart.plus(Period.of(0,0,borrowDuration)).toString()
+            borrowerInfo.add(endDate)
+            println("Added entry to borrowed book list")
+            println("Book Borrow Successfully")
+        }
+        println("Do you want to borrow another book? (Y/N): ")
+        optionYesNo = readLine().toString()
+        if(optionYesNo == "N" || optionYesNo == "n"){
+            canBorrow = false
+        }
+    }
+
+
+    println("Proceed to Book Record...")
+    println("Do you want to check borrowed book list?(Y/N): ")
+    optionYesNo = readLine().toString()
+    if(optionYesNo == "Y" || optionYesNo == "y"){
+        println("Book Borrowed List: ")
+        bookBorrowed.forEach(){
+            println("$it")
+        }
+        println(" ")
+        println("Checking borrower\'s info...")
+        println(" ")
+        if(borrowerInfo == null || borrowerInfo.size == 0){
+            println("No entry found.")
+        }else {
+            println("Borrower Info: ")
+            do{
+                println("${borrowerInfoColumn[column]}: ${borrowerInfo[counter]}")
+                ++ counter
+                ++ column
+                if(column == 6){
+                    column = column - 6
+                    println(" ")
+                }
+            }while(counter < borrowerInfo.size)
+        }
+    }
+}
--- src/main/kotlin/activity_01_j/Activity01J.kt
+++ src/main/kotlin/activity_01_j/Activity01J.kt
@@ -1,2 +1,79 @@
 package activity_01_j
+import java.time.LocalDate
 
+// grocery cart. user: cashier
+
+fun main(){
+    var groceryItemList = ArrayList<String>()
+    var isPerishable: Boolean? = null
+    var itemSKU: String? = null
+    var itemColor: String? = null
+    var itemType: String? = null
+    var itemOnSaleTotal: Int? = null
+    var ItemSoldTotal: Int? = null
+
+    // grocery cart var
+    var itemName: String? = null
+    var itemQuantity:Int? = null
+    var itemPrice:Float? = null
+    var optionsInput:String? = null
+    var itemCount = 0
+    var itemTotal:Float? = null
+    var totalCost:Float? = null
+    var itemPriceRounded = ""
+    var totalPerItemRounded = ""
+    var totalCostRounded = ""
+
+
+    // array list for carts
+    var itemsInCart = ArrayList<String>()
+    var itemsInCartQuantity = ArrayList<Int>()
+    var itemsInCartPrice = ArrayList<Float>()
+    var totalPerItem = ArrayList<Float>()
+
+
+    println("============")
+    println("Grocery Cart")
+    println("============")
+    do{
+        var addToCart = true
+        println("Enter item name: ")
+        itemName = readLine()!!.toString()
+        println("Quantity (per piece): ")
+        itemQuantity = readLine()!!.toInt()
+        println("Price (per item): ")
+        itemPrice = readLine()!!.toFloat()
+        if(itemPrice > 0 && itemQuantity > 0){
+            itemCount ++
+            itemsInCart.add(itemName)
+            itemsInCartQuantity.add(itemQuantity)
+            itemsInCartPrice.add(itemPrice)
+            itemTotal = itemQuantity * itemPrice
+            totalPerItem.add(itemTotal)
+        }else{
+            println("Error: Quantity and price can't be 0")
+            break
+        }
+        println("Item \"$itemName\" added to cart")
+        println("Would you like to add another item? (Y/N): ")
+        optionsInput = readLine().toString()
+        if(optionsInput == "N" || optionsInput == "n"){
+            addToCart = false
+        }
+    } while(addToCart)
+
+    println("Proceeds to Checkout...")
+    println("Items in Cart: ")
+    println("======================================================================")
+    for(index in 0 until itemCount){
+        itemPriceRounded = String.format("%.2f", itemsInCartPrice[index])
+        totalPerItemRounded = String.format("%.2f", totalPerItem[index])
+        println("${index+1}. ${itemsInCart[index]} | ${itemsInCartQuantity[index]} pc/s. | P $itemPriceRounded | Total: P $totalPerItemRounded")
+    }
+    println("======================================================================")
+    println("No. of items: $itemCount")
+    totalCost = totalPerItem.sum()
+    totalCostRounded = String.format("%.2f", totalCost)
+    println("Total Cost: P $totalCostRounded")
+
+}
