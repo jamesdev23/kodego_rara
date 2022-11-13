265b1c786de8dd5226d8a4311d577a7e03fb4650
jamesdev23 <jnrara0023@gmail.com>
2022-11-13 20:00:54 +0800

    updated activities

--- src/main/kotlin/activity_04_c/Activity04C.kt
+++ src/main/kotlin/activity_04_c/Activity04C.kt
@@ -1,32 +1,209 @@
 package activity_04_c
 
-class Poultry()
+import java.util.*
 
-class Fish()
+class Poultry(){
+    var itemName:String = ""
+    var poultryType:String = ""
+    var quantity:String = ""
+    var bestBeforeDate:Date = Date()
+}
 
-class CannedGoods()
+class Fish(){
+    var itemName:String = ""
+    var fishType:String = ""
+    var weight:Int = 0
+    var price:String = ""
+}
 
-class Snacks()
+class CannedGoods(){
+    var itemName:String = ""
+    var quantity:Int = 0
+    var brand:String = ""
+    var cannedGoodsType:String = ""
+    var price:String = ""
+    var sku:Int = 0
+    var manufactureDate: Date = Date()
+    var bestBeforeDate: Date = Date()
+    var manufacturerName:String = ""
+}
 
-class FrozenProducts()
+class Snacks(){
+    var itemName:String = ""
+    var brand:String = ""
+    var quantity:Int = 0
+    var snackType:String = ""
+    var price:String = ""
+    var sku:Int = 0
+    var manufactureDate: Date = Date()
+    var bestBeforeDate: Date = Date()
+    var manufacturerName:String = ""
+}
 
-class Fruits()
+class FrozenProducts(){
+    var itemName:String = ""
+    var brand:String = ""
+    var quantity:Int = 0
+    var frozenProductType:String = ""
+    var price:String = ""
+    var sku:Int = 0
+    var manufactureDate: Date = Date()
+    var bestBeforeDate: Date = Date()
+    var manufacturerName:String = ""
+}
 
-class Vegetables()
+class Fruits(){
+    var itemName:String = ""
+    var fruitType:String = ""
+    var weight:String = ""
+    var quantity:Int = 0
+}
 
-class Condiments()
+class Vegetables(){
+    var itemName:String = ""
+    var vegetableType:String = ""
+    var weight:String = ""
+    var quantity:Int = 0
+}
 
-class Furniture()
+class Condiments(){
+    var itemName:String = ""
+    var brand:String = ""
+    var quantity:Int = 0
+    var condimentType:String = ""
+    var price:String = ""
+    var sku:Int = 0
+    var manufactureDate: Date = Date()
+    var bestBeforeDate: Date = Date()
+    var manufacturerName:String = ""
+}
 
-class Appliances()
+class Furniture(){
+    var itemName:String = ""
+    var brand:String = ""
+    var itemSize:String = ""
+    var furnitureType:String = ""
+    var price:String = ""
+    var sku:Int = 0
+    var manufactureDate: Date = Date()
+    var manufacturerName:String = ""
+}
 
-class Clothes()
+class Appliances(){
+    var itemName:String = ""
+    var brand:String = ""
+    var itemSize:Int = 0
+    var applianceType:String = ""
+    var price:String = ""
+    var sku:Int = 0
+    var manufactureDate: Date = Date()
+    var manufacturerName:String = ""
+}
 
-class Pet()
+class Clothes(){
+    var itemName:String = ""
+    var brand:String = ""
+    var itemSize:Int = 0
+    var clothesType:String = ""
+    var price:String = ""
+    var sku:Int = 0
+    var manufactureDate: Date = Date()
+    var manufacturerName:String = ""
+}
 
-class SanitaryProducts()
+// assuming this is pet food and other pet items
+class Pet(){
+    var itemName:String = ""
+    var brand:String = ""
+    var quantity:Int = 0
+    var petItemType:String = ""
+    var price:String = ""
+    var sku:Int = 0
+    var manufactureDate: Date = Date()
+    var bestBeforeDate: Date = Date()
+    var manufacturerName:String = ""
+}
 
-class PersonalCare()
+class SanitaryProducts(){
+    var itemName:String = ""
+    var brand:String = ""
+    var quantity:Int = 0
+    var sanitaryProductType:String = ""
+    var price:String = ""
+    var sku:Int = 0
+    var manufactureDate: Date = Date()
+    var manufacturerName:String = ""
+}
+
+class PersonalCare(){
+    var itemName:String = ""
+    var brand:String = ""
+    var quantity:Int = 0
+    var personalCareItemType:String = ""
+    var price:String = ""
+    var sku:Int = 0
+    var manufactureDate: Date = Date()
+    var expirationDate: Date = Date()
+    var manufacturerName:String = ""
+}
+
+// additional 5 grocery item as class
+class Dairy{
+    var itemName:String = ""
+    var quantity:Int = 0
+    var brand:String = ""
+    var dairyType:String = ""
+    var price:String = ""
+    var sku:Int = 0
+    var manufactureDate: Date = Date()
+    var bestBeforeDate: Date = Date()
+}
+
+class DryGoods(){
+    var itemName:String = ""
+    var quantity:Int = 0
+    var brand:String = ""
+    var dryGoodsType:String = ""
+    var price:String = ""
+    var sku:Int = 0
+    var manufactureDate: Date = Date()
+    var bestBeforeDate: Date = Date()
+}
+
+class Bread(){
+    var itemName:String = ""
+    var weight:Int = 0
+    var brand:String = ""
+    var breadType:String = ""
+    var price:String = ""
+    var sku:Int = 0
+    var manufactureDate: Date = Date()
+    var bestBeforeDate: Date = Date()
+}
+class BreadSpread(){
+    var itemName:String = ""
+    var quantity:Int = 0
+    var brand:String = ""
+    var breadType:String = ""
+    var price:String = ""
+    var sku:Int = 0
+    var manufactureDate: Date = Date()
+    var bestBeforeDate: Date = Date()
+}
+
+class Kitchenware(){
+    var itemName:String = ""
+    var quantity:Int = 0
+    var itemSize:String = ""
+    var brand:String = ""
+    var kitchenwareType:String = ""
+    var price:String = ""
+    var sku:Int = 0
+    var manufactureDate: Date = Date()
+    var manufacturerName:String = ""
+}
+
+
 fun main(){
 
 }
