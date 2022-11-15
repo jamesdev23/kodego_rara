1bbac4e6111eeddd716f9f82c1dbfa8cfd0e48aa
jamesdev23 <jnrara0023@gmail.com>
2022-11-15 21:35:25 +0800

    updated activities

--- src/main/kotlin/exercise_oop/Exercise_oop.kt
+++ src/main/kotlin/exercise_oop/Exercise_oop.kt
@@ -3,36 +3,35 @@ open class Product {
         private set
     var price:Double = 0.0
 
-    constructor(name:String,price:Double){
+    constructor(name:String,price:Double) {
         this.name = name
         this.price = price
     }
 }
-data class Fruit(name:String, price:Double):Product(name,price){
+class Fruit(name:String, price:Double) : Product(name, price) {
     var quantity:Float = 0.0F
     var quantityUnits:String = "per Piece"
 }
-data class Shakes(name:String,price:Double):Product(name,price){
+class Shakes(name:String, price: Double):Product(name,price){
     var size: Any = ""
     var ingredients: ArrayList<String> = ArrayList()
     var addons: ArrayList<Any> = ArrayList()
 }
-data class Juices(name:String,price:Double):Product(name,price){
+class Juices(name:String,price:Double):Product(name,price){
     var ingredients: ArrayList<String> = ArrayList()
     var size: Any = ""
 }
-data class Sandwiches(name:String,price:Double):Product(name,price){
+class Sandwiches(name:String,price:Double):Product(name,price){
     var ingredients: ArrayList<String> = ArrayList()
     var addons: ArrayList<Any> = ArrayList()
     var breadType:Any = ""
 }
-data class Salads(name:String,price:Double):Product(name,price){
+class Salads(name:String,price:Double):Product(name,price){
     var ingredients: ArrayList<String> = ArrayList()
     var dressing:Any = ""
     var addons: ArrayList<Any> = ArrayList()
     var size:Any = ""
 }
-
 
 enum class OrderStatus{
     SENT_TO_THE_KITCHEN,
@@ -59,10 +58,13 @@ class Cart(var customer:Customer){
 }
 
 data class Customer(var address:String,var mobilenumber:String,
-    var firstname:String,var lastname:String)
+                    var firstname:String,var lastname:String)
 
 
 fun main(){
 
+
+
 }
+
 
