e5dae2e80b86db93dc8897647570b5fc321f3a99
James Nico Rara <113577747+jamesdev23@users.noreply.github.com>
2023-03-06 19:10:52 +0800

    updated activity 1-H

--- src/main/kotlin/activity_01_h/FindPrimeNumbers.kt
+++ src/main/kotlin/activity_01_h/FindPrimeNumbers.kt
@@ -1,8 +1,12 @@
 package activity_01_h
 
+import java.lang.Integer.max
+import java.lang.Integer.min
+
 // description: 2 ints. find higher value. outputs prime numbers between low -> high
 // scope: string, loops
-// updated: 3/5/23
+// updated: 3/6/23
+
 fun main() {
     print("Enter the 1st number: ")
     val num1:Int = readLine()!!.toInt()
@@ -10,30 +14,35 @@ fun main() {
     print("Enter the 2nd number: ")
     val num2:Int = readLine()!!.toInt()
 
-    val low = if (num1 < num2) num1 else num2
-    val high = if (num1 > num2) num1 else num2
+    findPrimes(num1, num2)
+}
 
-    if(num1 == num2){
-        if(!isPrime(num1)){
-            print("No prime numbers found")
-        } else {
-            print("Prime number: $num1")
-        }
+fun findPrimes(num1: Int, num2: Int) {
+    if (num1 < 0 || num2 < 0) {
+        println("No prime numbers found")
+        return
+    }
+    val low = min(num1, num2)
+    val high = max(num1, num2)
+
+    if (low == high && !isPrime(low)) {
+        println("No prime numbers found")
+        return
+    }
+    val primes = (low..high).filter { isPrime(it) }
+
+    if (primes.isNotEmpty()) {
+        println(primes.joinToString())
     } else {
-        for (num in low..high) {
-            if (isPrime(num)) {
-                print("$num, ")
-            }
-        }
+        println("No prime numbers found")
     }
 }
 
 fun isPrime(num: Int): Boolean {
     // quick if statement to return value when num is either below or equal to 1, 2 or 3
-    if (num < 1) {
-        return false
-    } else if (num == 1 || num == 2 || num == 3) {
-        return true
+    when {
+        num < 1 -> return false
+        num == 1 || num == 2 || num == 3 -> return true
     }
 
     // finding prime, with break if number is divisible
