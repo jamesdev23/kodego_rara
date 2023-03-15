e38224f996c482d50fda9be5dc84e8650de4135e
James Nico Rara <113577747+jamesdev23@users.noreply.github.com>
2023-03-15 17:55:05 +0800

    added some comments

--- src/test/kotlin/Activity01DTest.kt
+++ src/test/kotlin/Activity01DTest.kt
@@ -10,35 +10,29 @@ internal class MonetaryAppTest {
         val monetaryAmounts = mutableListOf(10.0, 20.0, 30.0, 40.0, 50.0)
         val divisor = 2
         val result = calculateResult(monetaryAmounts, divisor)
-
         // 2nd input
         val monetaryAmounts2 = mutableListOf(0.0, 0.0, 0.0, 0.0, 0.0)
         val divisor2 = 2
         val result2 = calculateResult(monetaryAmounts2, divisor2)
-
         // 3rd input
         val monetaryAmounts3 = mutableListOf(10.0, 20.0, 30.0, 40.0, 50.0)
         val divisor3 = 0
         val result3 = calculateResult(monetaryAmounts3, divisor3)
-
         // 4th input
         val monetaryAmounts4 = mutableListOf(0.0, 0.0, 0.0, 0.0, 0.0)
         val divisor4 = 0
         val result4 = calculateResult(monetaryAmounts4, divisor4)
-
         // 5th input
         val monetaryAmounts5 = mutableListOf(-100.0, -100.0, -100.0, -100.0, -100.0)
         val divisor5 = -100
         val result5 = calculateResult(monetaryAmounts5, divisor5)
-
-
         // 6th input
         val monetaryAmounts6 = mutableListOf(1_000_000.0, 1_000_000.0, 1_000_000.0, 1_000_000.0, 1_000_000.0)
         val divisor6 = 1_000
         val result6 = calculateResult(monetaryAmounts6, divisor6)
 
 
-        // test results
+        // test cases
 
         // success
         assertEquals(75.0, result)
