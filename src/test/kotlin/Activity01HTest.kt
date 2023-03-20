7821a72f301ec9bd540680ac8cd260b6488b5cbe
James Nico Rara <113577747+jamesdev23@users.noreply.github.com>
2023-03-20 15:17:04 +0800

    updated activity 1H test

--- src/test/kotlin/Activity01HTest.kt
+++ src/test/kotlin/Activity01HTest.kt
@@ -5,18 +5,18 @@ import org.junit.jupiter.api.Test
 internal class PrimeNumbers{
     @Test
     fun checkPrimeNumbersResult() {
-        val answer1to10:ArrayList<Int> = arrayListOf(1,2,3,5,7)
-        val answer1to1:ArrayList<Int> = arrayListOf(1)
-        val answerNegativeNumbers: ArrayList<Int> = arrayListOf()
+        val expected1: ArrayList<Int> = arrayListOf(1,2,3,5,7)
+        val expected2: ArrayList<Int> = arrayListOf(1)
+        val expected3: ArrayList<Int> = arrayListOf()
 
         // success
-        assertEquals(answer1to10, findPrimes(1,10))
-        assertEquals(answer1to1, findPrimes(1,1))
-        assertEquals(answerNegativeNumbers, findPrimes(-1,-10))
+        assertEquals(expected1, findPrimes(1,10))
+        assertEquals(expected2, findPrimes(1,1))
+        assertEquals(expected3, findPrimes(-1,-10))
 
         // fail
-        assertEquals(answer1to1, findPrimes(1, 10))
-        assertEquals(answer1to10, findPrimes(1,1))
-        assertEquals(answer1to1, findPrimes(-1,-10))
+        assertEquals(expected2, findPrimes(1, 10))
+        assertEquals(expected1, findPrimes(1,1))
+        assertEquals(expected2, findPrimes(-1,-10))
     }
 }
